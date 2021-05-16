80f091dadd27587e6a03ef2d9cfb802b70b5b6a3
yasineryigit <yasineryigit737@gmail.com>
2021-05-16 04:47:40 +0300

    BarList sorted | BarChart Graphic drawed by year

--- src/sample/controllers/BarChartSceneController.java
+++ src/sample/controllers/BarChartSceneController.java
@@ -1,16 +1,24 @@
 package sample.controllers;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 import sample.Bar;
 import sample.model.BarChartModel;
 
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.Collections;
 import java.util.Comparator;
@@ -26,29 +34,53 @@ public class BarChartSceneController implements Initializable {
     @FXML
     private CategoryAxis categoryAxis;
     @FXML
+    private NumberAxis numberAxis;
+    @FXML
     private Label labelTitle;
+    XYChart.Series<String, Integer> series = new XYChart.Series<>();
+    int i = 1;
+    Timeline tl = new Timeline();
 
     private ObservableList<String> countries = FXCollections.observableArrayList();
     private ObservableList<Integer> values = FXCollections.observableArrayList();
     private ObservableList<String> years = FXCollections.observableArrayList();
 
 
-    public void drawGraphic(BarChartModel barChartModel){
+    public void drawGraphic(BarChartModel barChartModel) {
         //labelTitle.setText(barChartModel.getTitle());
-        for(Bar bar : barChartModel.getBarList()){//seride kullanmak üzere countries, values, years listelerini doldurur
+        for (Bar bar : barChartModel.getBarList()) {//seride kullanmak üzere countries, values, years listelerini doldurur
             countries.add(bar.getCountry());
             values.add(bar.getValue());
-            if(!years.contains(String.valueOf(bar.getValue()))){
-                years.add(String.valueOf(bar.getYear()));
-            }
-            barGraphic.setTitle(String.valueOf(bar.getYear()));//yılı yazdır
+            years.add(String.valueOf(bar.getYear()));
+
+            //barGraphic.setTitle(String.valueOf(bar.getYear()));//yılı yazdır
         }
-        XYChart.Series<String,Integer> series = new XYChart.Series<>();// seri objesi oluşturup içine country ve values objelerini attık
-        for (int i = 0; i < barChartModel.getBarList().size(); i++) {
-            series.getData().add(new XYChart.Data<>(countries.get(i), Integer.parseInt(values.get(i).toString())));
-        }
-        barGraphic.getData().add(series);//seriyi grafiğe bağladık
-        categoryAxis.setCategories(years);
+        /*
+        for (int i = 0; i < barChartModel.getBarList().size(); i++) {//seriyi doldur
+
+        }*/
+
+
+        tl.getKeyFrames().add(
+                new KeyFrame(Duration.millis(10),//100ms'de bir
+                        new EventHandler<ActionEvent>() {
+                            @Override public void handle(ActionEvent actionEvent) {
+                                if(i<barChartModel.getBarList().size()){
+                                    barGraphic.setTitle(String.valueOf(barChartModel.getBarList().get(i).getYear()));
+                                    series.getData().add(new XYChart.Data<>(countries.get(i), values.get(i)));
+                                    barGraphic.getData().add(series);//seriyi grafiğe bağladık
+                                    i++;
+                                }else{
+                                    //timeline'ı durdur TODO
+                                }
+
+
+                            }
+                        }
+                ));
+        tl.setCycleCount(Animation.INDEFINITE);
+        tl.setAutoReverse(true);
+        tl.play();
 
     }
 
@@ -56,15 +88,16 @@ public class BarChartSceneController implements Initializable {
         Collections.sort(barList, new Comparator<Bar>() {
             @Override
             public int compare(Bar o1, Bar o2) {
-                return o1.getYear()-(o2.getYear());
+                return o1.getYear() - (o2.getYear());
             }
         });
 
     }
 
 
-        @Override
+    @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
 
     }
 
